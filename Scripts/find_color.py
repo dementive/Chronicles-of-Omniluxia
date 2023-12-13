1605f9450bedf4571a82a293c548365729f2dc93
dementive <nathanvandrei@yahoo.com>
2023-12-13 03:14:22 -0500

    Split up impassable provinces

--- Scripts/find_color.py
+++ Scripts/find_color.py
@@ -5,9 +5,9 @@ img = Image.open('../map_data/provinces.png')
 
 img_array = np.array(img)
 
-color = (131, 12, 184)
+color = (255, 22, 253)
 coords = np.where(np.all(img_array == color, axis=-1))
 
-found_coords = list(zip(coords[0], coords[1]))
+found_coords = list(zip(coords[1], coords[0]))
 
 print(found_coords)
